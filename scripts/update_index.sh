6a89d45f4e5f217faa324bbb2fd0f190929bcc2e
Santiago M. Mola <santiago.mola@bitsnbrains.net>
2013-09-26 00:05:51 +0200

    Sort index alphabetically.

--- scripts/update_index.sh
+++ scripts/update_index.sh
@@ -2,7 +2,7 @@
 
 sed -i -e '/^<!-- test -->/d' index.html
 
-for t in $(find tests -name 'index.html') ; do
+for t in $(find tests -name 'index.html' | sort) ; do
 	t=${t/index.html/}
-	sed -i -e "s:^\(<\!-- END INDEX -->\)$:<!-- test --><a href="/$t">$t</a>\n\1:g" index.html
+	sed -i -e "s:^\(<\!-- END INDEX -->\)$:<!-- test --><a href="/$t">$t</a><br>\n\1:g" index.html
 done
